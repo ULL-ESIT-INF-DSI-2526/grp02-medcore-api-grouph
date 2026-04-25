2629bd5529dbbd06a0551e96355e3f6d0cfa197c
Francisco Fdez <alu0101427324@ull.edu.es>
2026-04-25 19:00:21 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/JMUAIOxt)
+# Example of REST API
+
+This is an example of a REST API for notes management based on MongoDB, Node.js and Express.
---
+++ src/routers/post.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+import {Note} from '../models/note';
+
+export const postRouter = express.Router();
+
+postRouter.post('/notes', async (req, res) => {
+  const note = new Note(req.body);
+
+  try {
+    await note.save();
+    res.status(201).send(note);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
