8234e2aaa26070574e8703e0c765eee071b710b2
Eduardo Socas <alu0101404622@ull.edu.es>
2026-05-09 17:10:27 +0000

    DefaulrRouter minor changes

--- src/routers/default.ts
+++ src/routers/default.ts
@@ -3,5 +3,8 @@ import express from 'express';
 export const defaultRouter = express.Router();
 
 defaultRouter.use((_, res) => {
-  res.status(501).send();
+  res.status(501).send({
+    help: 'Check our documentation for available endpoints',
+    link: 'https://medcore-api-grouph.onrender.com/api-docs'
+   });
 });
